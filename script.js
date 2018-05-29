5d94c54065758086d7a22d8a72bf959beb65e95c
Shawn Levin <shawnlevin22@gmail.com>
2018-05-28 22:20:17 -0700

    styled buttons with bootstrap

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function dropdownList() {
+  document.getElementById("listDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
 
     var dropdowns = document.getElementsByClassName("dropdown-content");
